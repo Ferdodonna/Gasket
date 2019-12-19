2e71541af446db01c087c0fe335b5b4f99c32c41
Silvano Dami <51679922+Ferdodonna@users.noreply.github.com>
2019-12-19 20:48:17 +0000

    Update gasket.pl

--- gasket.pl
+++ gasket.pl
@@ -1,25 +1,16 @@
 
-/*
-TODO:
-- convert to cplfd (clpfd with floats?)
-- complete
-- validate
-
-*/
-
 :- use_module(library(clpfd)).
 
 :- dynamic circle/2.
 % circle(radius,coordinates)
 :- dynamic line/2.
-
 % line(gradient,coordiantes)
 
 
 
 % checks if that each of the 3 circles touches each of the other 2 in one exactly one point
 valid(circle(R1,C1),circle(R2,C2),circle(R3,C3)) :-
-  dist(C1,C2,D12), % need to round D12, D13, D23
+  dist(C1,C2,D12),
   dist(C1,C3,D13),
   dist(C2,C3,D23),
   R1+R2 #= D12,
@@ -49,15 +40,7 @@ crossing(line(Grad1,X1-Y1),line(Grad2,X2-Y2),X-Y) :-
 createLine(X1-Y1,X2-Y2,line(Gradient,X1-Y1)) :-
   Gradient is (Y1-Y2) / (X1-X2).
 
-% finds the middle of 3 circles, meaning the point that has the same distance to each of the circle fringes
-middle(circle(R1,C1),circle(R2,C2),circle(R3,C3),M) :-
-  touchpoint(circle(R1,C1),circle(R2,C2),TP12),
-  touchpoint(circle(R2,C2),circle(R3,C3),TP23),
-  createLine(C1,TP23,L23),
-  createLine(C3,TP12,L12),
-  crossing(L12,L23,M).
-
-% finds the touching point of two circles, provided they touch (no intersection)
+% finds the touching point of two circles, provided they touch
 touchpoint(circle(R1,C1),circle(R2,C2),TP) :-
   C1 = X1-_,
   C2 = X2-_,
@@ -72,37 +55,54 @@ touchpoint(circle(R1,C1),circle(R2,C2),TP) :-
 
 % move a point a certain distance given a gradient
 % move(D,line(G,P),T) => T is the point P moved a distance of D along the gradient G
-move(D,line(Grad,X-Y),RX-RY) :-
-  D >= 0,
-  XD is sqrt((D * D) / (Grad + 1)),
-  YD is sqrt((D * D) - (XD * XD)),
-  RX is X + XD,
-  RY is Y + YD.
+move(D,line(G,X-Y),Rx-Ry) :-
+  Tx is sqrt((D * D) / (1 + G * G)),
+  Ty is G * Tx,
+  (D < 0 ->
+    Rx is X - Tx,
+    Ry is Y - Ty
+  ;
+    Rx is X + Tx,
+    Ry is Y + Ty).
 
+% Mittelpunkt zwischen zwei Punkten
+midpoint(X1-Y1,X2-Y2,X-Y) :-
+  X is (X1 + X2) / 2,
+  Y is (Y1 + Y2) / 2.
 
+% berechnet die Mitte nach deiner Vermutung über den Dreiecksschwerpunkt
+middle(C1,C2,C3,S1,S2,S3) :-
+  touchpoint(C1,C2,TP12),
+  touchpoint(C2,C3,TP23),
+  touchpoint(C3,C1,TP31),
 
+  midpoint(TP12,TP31,M1),
+  midpoint(TP12,TP23,M2),
+  midpoint(TP23,TP31,M3),
 
+  createLine(M1,TP23,L1),
+  createLine(M2,TP31,L2),
+  createLine(M3,TP12,L3),
 
+  crossing(L1,L2,S1),
+  crossing(L2,L3,S2),
+  crossing(L3,L1,S3).
 
+/*
+Testlauf:
 
+?- C1 = circle(2,2-0), C2 = circle(2,(-2)-0), dist(0-5,2-0,X), R is X - 2, C3 = circle(R,0-5), middle(C1,C2,C3,S1,S2,S3).
+C1 = circle(2, 2-0),
+C2 = circle(2, -2-0),
+X = 5.385164807134504,
+R = 3.3851648071345037,
+C3 = circle(3.3851648071345037, 0-5),
+S1 = -2.254972746618902e-16-1.2379689211803457,
+S2 = -7.401486830834378e-17-1.237968921180346,
+S3 = -7.401486830834377e-17-1.2379689211803457.
 
+Also entweder ist mein code falsch oder das stimmt so nicht ganz, da ja der Schnittpunkt S1 anders ist als die anderen auch wenn nur minimal.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
 
 %
